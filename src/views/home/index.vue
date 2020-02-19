9848b16805a6f170566c5c7237aef150eff23204
linxi <1242838858@qq.com>
2020-02-19 21:06:52 +0800

    主页布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,9 +1,17 @@
 <template>
-  <div class="main">home</div>
+  <div class="home">
+    <el-container>
+      <el-aside width="200px">左侧</el-aside>
+      <el-container>
+        <el-header>头部</el-header>
+        <el-main>中间</el-main>
+      </el-container>
+    </el-container>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
-<style>
+<style lang="less"  scoped>
 </style>
