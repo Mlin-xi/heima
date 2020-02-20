4e8df2d2ff0b997e8747ce6eca04378951a8321e
linxi <1242838858@qq.com>
2020-02-20 15:53:30 +0800

    头部及下拉菜单

---
+++ src/components/home/layout-header.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="layout-header">
+    <el-row class="title" type="flex" justify="space-between">
+      <!-- spans 给宽度 -->
+      <el-col :span="6" class="left">
+        <i class="el-icon-s-unfold icon"></i>
+        <span>江苏传智播客教育科技股份有限公司</span>
+      </el-col>
+      <el-col :span="5.5" class="right">
+          <el-input class="input"  prefix-icon="el-icon-search" placeholder="请输入搜素的文章内容"></el-input>
+          <span class="sp">消息</span>
+        <img class="head-img" src="../../assets/img/avatar.jpg" alt />
+        <el-dropdown style="margin:0" trigger="click">
+          <span class="el-dropdown-link">
+            大佬
+          <i class="el-icon-arrow-down el-icon--right"></i>
+          </span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item icon="el-icon-plus">个人信息</el-dropdown-item>
+            <el-dropdown-item icon="el-icon-circle-plus">git地址</el-dropdown-item>
+            <el-dropdown-item icon="el-icon-circle-plus-outline">退出</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scope>
+.layout-header {
+  background-color: #fff;
+  height: 50px;
+  .title {
+    line-height: 50px;
+    .left{
+        display: flex;
+        align-items: center;
+        .icon {
+      font-size: 22px;
+      margin: 0 8px 0;
+    }
+    }
+    .right{
+        position: relative;
+        display: flex;
+        align-items: center;
+        margin-right: 10px;
+        .head-img {
+            border-radius: 50%;
+            height: 40px;
+            margin: 0 8px 0
+        }
+        .sp{
+            margin: 0 0 0 8px
+        }
+        .input{
+            width: 200px;
+        }
+    }
+  }
+}
+</style>
